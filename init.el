573b52ab0c43ee03be03c9169c83accb0e25f9e8
Sho Hashimoto <hashimoto@shokai.org>
2012-04-26 09:25:10 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,44 @@
+(add-to-list 'load-path "~/.emacs.d")
+
+(load "~/.emacs.d/scratch.el")
+
+;;ruby-mode
+(add-to-list 'auto-mode-alist '("\\.r[bu]$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\Rakefile$" . ruby-mode))
+
+;;haml-mode
+(require 'haml-mode)
+(add-to-list 'auto-mode-alist '("\\.haml$" . haml-mode))
+
+;;sass-mode
+(require 'sass-mode)
+(add-to-list 'auto-mode-alist '("\\.sass$" . sass-mode))
+(add-to-list 'auto-mode-alist '("\\.scss$" . sass-mode))
+(add-to-list 'auto-mode-alist '("\\.css$" . sass-mode))
+
+;;coffee-script-mode
+(require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+
+;;scala-mode
+(require 'scala-mode-auto)
+
+;;auto complete
+(require 'auto-complete)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(require 'auto-complete-config)
+(ac-config-default)
+(global-auto-complete-mode "enter")
+(define-key ac-complete-mode-map "\C-n" 'ac-next)
+(define-key ac-complete-mode-map "\C-p" 'ac-previous)
+
+(require 'yasnippet-bundle)
+(yas/initialize)
+
+(require 'ajc-java-complete-config)
+(add-hook 'java-mode-hook 'ajc-java-complete-mode)
+(add-hook 'find-file-hook 'ajc-4-jsp-find-file-hook)
+
+(put 'set-goal-column 'disabled nil)
