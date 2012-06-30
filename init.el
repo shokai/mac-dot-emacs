cd36c825792aa64cff448435e758b24d0958ca99
Sho Hashimoto <hashimoto@shokai.org>
2012-06-30 16:06:17 +0900

    set coffee-script.el indent level 2

--- init.el
+++ init.el
@@ -20,6 +20,13 @@
 (require 'coffee-mode)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+(defun coffee-custom ()
+  "coffee-mode-hook"
+  (set (make-local-variable 'tab-width) 2)
+  (setq coffee-tab-width 2)
+  )
+(add-hook 'coffee-mode-hook
+          (lambda() (coffee-custom)))
 
 ;;jade-mode
 (require 'sws-mode)
