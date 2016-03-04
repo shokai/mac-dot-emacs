803f1cb9fee22cf4fd92188a91f72c9df1ea0177
Sho Hashimoto <hashimoto@shokai.org>
2016-03-04 21:00:48 +0900

    enable js2-jsx-mode

--- init.el
+++ init.el
@@ -80,6 +80,8 @@
 (add-to-list 'auto-mode-alist '("\\.es6$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx$" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
 
 (require 'elixir-mode)
 (add-to-list 'auto-mode-alist '("\\.exs$" . elixir-mode))
