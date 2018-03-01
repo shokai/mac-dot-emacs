b6245dafa015d6fc85a6f8f44a1857ded022fd4a
Sho Hashimoto <hashimoto@shokai.org>
2018-03-01 10:07:11 +0900

    rmeove elpa dir

--- elpa/sws-mode-20150317.1245/sws-mode-autoloads.el
+++
@@ -1,21 +0,0 @@
-;;; sws-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "sws-mode" "sws-mode.el" (22626 15163 0 0))
-;;; Generated autoloads from sws-mode.el
-
-(autoload 'sws-mode "sws-mode" "\
-Major mode for editing significant whitespace files
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; sws-mode-autoloads.el ends here
